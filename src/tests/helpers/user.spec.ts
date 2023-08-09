4ff24e91d5d16f72bb985ffb9879421c27139432
binhtt20 <ttbinhct.96@gmail.com>
2023-08-09 23:00:09 +0700

    done user spec

--- src/tests/helpers/user.spec.ts
+++ src/tests/helpers/user.spec.ts
@@ -4,67 +4,90 @@ import { CreateUserDto, UpdateUserDto } from '../../models/user-shopping';
 
 const request = supertest(app);
 describe('User handler specification', () => {
-  let token: string;
-  let userId: string;
+
+  const _createUserInput: CreateUserDto = {
+    username: 'binhtt22',
+    firstName: 'binh',
+    lastName: 'tran',
+    password: 'abc123',
+  };
+
+  async function createFunc(input: CreateUserDto) : Promise<any> {
+    const result = await request.post('/user').send(input);
+    return { token: result.body.token, userId: result.body.id, statusCode: result.statusCode };
+  }
+
+  async function deleteFunc(id: number, token: string) : Promise<any> {
+    const result = await request
+      .delete(`/user/${id}`)
+      .set('Authorization', 'Bearer ' + token);
+    return { statusCode: result.statusCode };
+  }
+
 
   it('should create user successfully.', async (done) => {
-    const userData: CreateUserDto = {
-      username: 'binhtt22',
+    const createUserInput: CreateUserDto = {
+      username: 'binhtt123',
       firstName: 'binh',
       lastName: 'tran',
       password: 'abc123',
     };
-    const res = await request.post('/user').send(userData);
 
-    const { body, statusCode } = res;
-    token = body.token;
-    userId = body.id;
-
+    const { userId, token, statusCode } = await createFunc(createUserInput);
+    await deleteFunc(userId, token);
     expect(statusCode).toBe(200);
     done();
   });
 
-  // it('should update user successfully.', async (done) => {
-  //   const input: UpdateUserDto = {
-  //     firstName: 'binh',
-  //     lastName: 'tran',
-  //   };
+  it('should update user successfully.', async (done) => {
+    const input: UpdateUserDto = {
+      firstName: 'binh',
+      lastName: 'tran',
+    };
 
-  //   const res = await request
-  //     .put(`/user/${userId}`)
-  //     .send(input)
-  //     .set('Authorization', 'Bearer ' + token);
+    const { userId, token } = await createFunc(_createUserInput);
 
-  //   expect(res.statusCode).toBe(200);
-  //   done();
-  // });
-
-  it('should gets all users successfully.', async (done) => {
     const res = await request
-    .get('/user')
-    .set('Authorization', 'Bearer ' + token);
+      .put(`/user/${userId}`)
+      .send(input)
+      .set('Authorization', 'Bearer ' + token);
 
+    await deleteFunc(userId, token);
     expect(res.statusCode).toBe(200);
     done();
   });
 
-  // it('should get user by id successfully.', async (done) => {
-  //   const res = await request
-  //   .get(`/user/${userId}`)
-  //   .set('Authorization', 'Bearer ' + token);
+  it('should gets all users successfully.', async (done) => {
+    const { userId, token } = await createFunc(_createUserInput);
 
-  //   expect(res.statusCode).toBe(200);
-  //   done();
-  // });
+    const res = await request
+    .get('/user')
+    .set('Authorization', 'Bearer ' + token);
 
-  // afterAll(async () => {
-  //     it('should delete user successfully.', async (done) => {
-  //       const res = await request
-  //       .delete(`/user/${userId}`)
-  //       .set('Authorization', 'Bearer ' + token);
+    await deleteFunc(userId, token);
+    expect(res.statusCode).toBe(200);
+    done();
+  });
 
-  //       expect(res.statusCode).toBe(200);
-  //       done();
-  //   });
-  // })
+  it('should get user by id successfully.', async (done) => {
+    const { userId, token } = await createFunc(_createUserInput);
+
+    const res = await request
+    .get(`/user/${userId}`)
+    .set('Authorization', 'Bearer ' + token);
+
+    await deleteFunc(userId, token);
+    expect(res.statusCode).toBe(200);
+    done();
+  });
+
+  it('should delete user successfully.', async (done) => {
+    
+    const { userId, token } = await createFunc(_createUserInput);
+    const res = await deleteFunc(userId, token);
+    
+    expect(res.statusCode).toBe(200);
+    done();
+  });
+
 });
